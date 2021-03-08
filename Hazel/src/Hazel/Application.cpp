a88ff9152d820679799ca6dc79898e7c7043a225
wk1093 <wyattk1093@gmail.com>
2021-03-07 20:10:21 -0800

    Input Polling

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -6,6 +6,8 @@
 
 #include <glad/glad.h>
 
+#include "Input.h"
+
 namespace Hazel {
 
 	Application* Application::s_Instance = nullptr;
@@ -54,6 +56,9 @@ namespace Hazel {
 
 			for (Layer* layer : m_LayerStack)
 				layer->OnUpdate();
+
+			auto [x, y] = Input::GetMousePosition();
+			HZ_CORE_TRACE("{0}, {1}", x, y);
 
 			m_Window->OnUpdate();
 		}
